ee0baede8e00a9d3c0716ecceb170a0c49907942
Sujanraj0306 <sujanraj.seyarkAI@gmail.com>
2025-07-21 14:45:45 +0530

    updated mini project

--- minor project/script.js
+++ minor project/script.js
@@ -15,3 +15,56 @@ function calculate() {
     display.value = 'Error';
   }
 }
+
+function squareRoot() {
+  try {
+    display.value = Math.sqrt(eval(display.value));
+  } catch {
+    display.value = 'Error';
+  }
+}
+
+function square() {
+  try {
+    let val = eval(display.value);
+    display.value = val * val;
+  } catch {
+    display.value = 'Error';
+  }
+}
+
+function log() {
+  try {
+    display.value = Math.log10(eval(display.value));
+  } catch {
+    display.value = 'Error';
+  }
+}
+
+function sin() {
+  try {
+    display.value = Math.sin(toRadians(eval(display.value)));
+  } catch {
+    display.value = 'Error';
+  }
+}
+
+function cos() {
+  try {
+    display.value = Math.cos(toRadians(eval(display.value)));
+  } catch {
+    display.value = 'Error';
+  }
+}
+
+function tan() {
+  try {
+    display.value = Math.tan(toRadians(eval(display.value)));
+  } catch {
+    display.value = 'Error';
+  }
+}
+
+function toRadians(degrees) {
+  return degrees * (Math.PI / 180);
+}
